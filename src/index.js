7fe4de37c3dde9252be0cd965fb586414c7dba58
meetdobariya01 <meetdobariya87@gmail.com>
2025-12-31 09:04:17 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/home/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/header/header';
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+      Home Component
+    </div>
+  )
+}
+
+export default Home;
